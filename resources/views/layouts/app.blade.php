5d63a3815719b551eba84a7f9723b1a9976c4aae
Emanuel Millan <emanuelmillan455@gmail.com>
2022-03-30 14:09:13 -0500

    Se agrego el script encargado de los iconos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
 
-    {{-- Script necesarios para el funcionamiento de jquery, bootstrap y datatables --}}
+    {{-- Script necesarios para el funcionamiento de jquery, bootstrap, datatables y fontawesome --}}
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
@@ -19,7 +19,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-
+    <script src="https://kit.fontawesome.com/f27e946422.js" crossorigin="anonymous"></script>
 
     <title>Empleados</title>
 </head>
